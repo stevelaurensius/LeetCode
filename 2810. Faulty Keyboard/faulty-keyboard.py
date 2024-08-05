1991de9f598ceaf6ad6f9863af1c69f7f9f26e9f
stevelaurensius <steve.laurensius+github@gmail.com>
2024-08-05 20:13:17 +0700

    1st commit

--- 2810. Faulty Keyboard/faulty-keyboard.py
+++ 2810. Faulty Keyboard/faulty-keyboard.py
@@ -2,8 +2,8 @@ class Solution:
     def finalString(self, s: str) -> str:
         result_ = ''
         for i in s:
-            if i == 'i':
-                result_ = result_[::-1]
-            else:
+            if i != 'i':
                 result_ += i
+            else:
+                result_ = result_[::-1]
         return result_
