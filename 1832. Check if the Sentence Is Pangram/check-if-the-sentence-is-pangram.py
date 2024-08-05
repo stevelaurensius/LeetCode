b855393dad3cc1568d9ba2839617c8a868b30bb0
stevelaurensius <steve.laurensius+github@gmail.com>
2024-08-06 03:09:16 +0700

    1st commit

---
+++ 1832. Check if the Sentence Is Pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence):
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        sentence = set(sentence)
+        for char in sentence:
+            if char in alphabet:
+                alphabet.remove(char)
+        return len(alphabet) == 0
