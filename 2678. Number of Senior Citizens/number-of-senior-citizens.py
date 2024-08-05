375b23bcc6f76dfe7c5b713754317288251d0af1
stevelaurensius <steve.laurensius+github@gmail.com>
2024-08-05 17:05:13 +0700

    1st commit

---
+++ 2678. Number of Senior Citizens/number-of-senior-citizens.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        result = 0
+        for detail in details:
+            if int(detail[11:13]) > 60:
+                result += 1
+        return result
